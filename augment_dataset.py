d923f1380e51e032e929c47df03cd3a937f0062a
Hubert1225 <hubert.baran@omniscopy.com>
2024-04-26 19:59:49 +0200

    add handling profile directories in script

--- augment_dataset.py
+++ augment_dataset.py
@@ -3,6 +3,16 @@ dataset. It mixes each audio file with a random noise audio file
 
 Args:
     data_dir: path to the directory with sound files (WAV)
+        the directory at path data_dir should have the following structure:
+        data_dir_name
+        |
+        |---profile_name_1
+           |
+           |---file_1.wav
+           |---file_2.wav
+           |--- ...
+        |---profile_name_2
+        |--- ...
     noises_dir: path to the directory with noises files (MP3)
     output_dir: path to the directory to put output files in
     --noise_scale: the target ratio between the noise max amplitude and the sound max amplitude,
@@ -68,6 +78,32 @@ def load_noises() -> list[AudioSegment]:
     ]
 
 
+def process_profile(profile_name: str, noises: list[AudioSegment]):
+    """For a profile dir, creates profile dir
+    in the output dir, augments profile sounds with noises
+    and adds augmented sounds to the output profile
+    """
+    os.makedirs(os.path.join(args.output_dir, profile_name))
+    profile_path = os.path.join(args.data_dir, profile_name)
+    for fname in os.listdir(profile_path):
+
+        if not fname.endswith(".wav"):
+            continue
+
+        sound = AudioSegment.from_wav(
+            os.path.join(profile_path, fname)
+        )
+        random_noise = random.choice(noises)
+        mixed_sound = mix_sound_and_noise(
+            sound=sound, noise=random_noise, noise_scale=float(args.noise_scale)
+        )
+
+        mixed_sound.export(
+            out_f=os.path.join(args.output_dir, profile_name, fname),
+            format="wav"
+        )
+
+
 if __name__ == '__main__':
 
     # load noises
@@ -77,20 +113,8 @@ if __name__ == '__main__':
 
     # creating mixed sounds and saving to output_dir
     print("Creating mixed sounds ...")
-    for fname in tqdm(os.listdir(args.data_dir)):
+    for profile_name in tqdm(os.listdir(args.data_dir)):
 
-        if not fname.endswith(".wav"):
-            continue
-
-        sound = AudioSegment.from_wav(
-            os.path.join(args.data_dir, fname)
-        )
-        random_noise = random.choice(noises)
-        mixed_sound = mix_sound_and_noise(
-            sound=sound, noise=random_noise, noise_scale=float(args.noise_scale)
-        )
-
-        mixed_sound.export(
-            out_f=os.path.join(args.output_dir, fname),
-            format="wav"
+        process_profile(
+            profile_name=profile_name, noises=noises
         )
